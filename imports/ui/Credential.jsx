59bf30ffd7292c50671219a17437cf121e970391
icon6669 <kevin@kevinjemison.com>
2016-07-17 14:27:51 -0500

    added selection and deletion elements to credentials and segments

--- imports/ui/Credential.jsx
+++ imports/ui/Credential.jsx
@@ -1,10 +1,40 @@
 import React, { Component, PropTypes } from 'react';
+import {Credentials} from '../api/credentials.js';
 
 // Credential component - represents a single web services credential, record contains username, logincompany, and sharedsecret
 export default class Credential extends Component {
+  toggleChecked() {
+    // Set the checked property to the opposite of its current value
+    Credentials.update(this.props.credential._id, {
+      $set: { checked: !this.props.credential.checked },
+    });
+  }
+
+  deleteThisCredential() {
+    Credentials.remove(this.props.credential._id);
+  }
+
   render() {
+
+// Give credentials a different className when they are checked off,
+// so that we can style them nicely in CSS
+const credentialClassName = this.props.credential.checked ? 'checked' : '';
+
     return (
-      <li>{this.props.credential.logincompany} - {this.props.credential.username}</li>
+      <li className={credentialClassName}>
+      <button className="delete" onClick={this.deleteThisCredential.bind(this)}>
+        &times;
+      </button>
+
+      <input
+        type="checkbox"
+        readOnly
+        checked={this.props.credential.checked}
+        onClick={this.toggleChecked.bind(this)}
+      />
+
+      <span className="credentialLabel">{this.props.credential.logincompany} - {this.props.credential.username}</span>
+      </li>
     );
   }
 }
--- imports/ui/Segment.jsx
+++ imports/ui/Segment.jsx
@@ -1,14 +1,44 @@
 import React, { Component, PropTypes } from 'react';
+import {Segments} from '../api/segments.js';
 
-// Segment component - represents a single segment
+// Segment component - represents a single web services segment, record contains username, logincompany, and sharedsecret
 export default class Segment extends Component {
+  toggleChecked() {
+    // Set the checked property to the opposite of its current value
+    Segments.update(this.props.segment._id, {
+      $set: { checked: !this.props.segment.checked },
+    });
+  }
+
+  deleteThisSegment() {
+    Segments.remove(this.props.segment._id);
+  }
+
   render() {
+
+// Give segments a different className when they are checked off,
+// so that we can style them nicely in CSS
+const segmentClassName = this.props.segment.checked ? 'checked' : '';
+
     return (
-      <li>{this.props.segment.name}</li>
+      <li className={segmentClassName}>
+      <button className="delete" onClick={this.deleteThisSegment.bind(this)}>
+        &times;
+      </button>
+
+      <input
+        type="checkbox"
+        readOnly
+        checked={this.props.segment.checked}
+        onClick={this.toggleChecked.bind(this)}
+      />
+
+      <span className="segmentLabel">{this.props.segment.name}</span>
+      </li>
     );
   }
 }
- 
+
 Segment.propTypes = {
   // This component gets the segment to display through a React prop.
   // We can use propTypes to indicate it is required
